8914753b41d80c050aa7203a6da418391c6a2ced
杨子豪 <beiding110@gmail.com>
2025-04-01 15:25:51 +0800

    feat: 将测试文件独立；

---
+++ test.js
@@ -0,0 +1,30 @@
+import BidComparator from './index.js';
+
+let comparator = new BidComparator();
+
+comparator.textCompareProgressHandlerFactory = function (id) {
+    return function (num, str) {
+        console.log(id, num, str);
+    };
+};
+
+comparator.imageCompareProgressHandlerFactory = function (id) {
+    return function (num, str) {
+        console.log(id, num, str);
+    };
+};
+
+comparator.preload('./docs/g2-3.pdf');
+
+comparator
+    .processFiles(
+        [
+            './docs/g2-1.pdf', 
+            './docs/g2-2.pdf', 
+            // './docs/g2-3.pdf',
+        ], 
+        // './docs/g2-exclude.pdf'
+    )
+    .then((res) => {
+        console.log(res);
+    });
