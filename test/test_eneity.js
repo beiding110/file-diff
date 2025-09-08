d42921d1b92e267e5436d8b68a5e1b481eacecac
杨子豪 <beiding110@gmail.com>
2025-09-08 14:51:58 +0800

    feat: 调整实体提取方法单元测试文件；

--- test/test_eneity.js
+++ test/test_eneity.js
@@ -2,11 +2,14 @@
 const EntityExtracter = require('../utils/EntityExtracter');
 
 const text = `
+
 `;
 
 console.time();
 
 const extracter = new EntityExtracter(text);
+
+console.log(extracter._tags);
 
 const result = extracter.extract();
 
